7f57cff04ac416e22fbf335e0c4c2c39382f187b
prajapatisumit8900-collab <prajapatisumit8900@gmail.com>
2026-09-20 05:45:12 -0700

    update

---
+++ js/firebase-config.js
@@ -0,0 +1,21 @@
+// ==========================================
+// GyanAstra User - Firebase Configuration
+// ==========================================
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqcL86o1UwOGn9wcLQqVhDF0lzJQD1NG8",
+  authDomain: "gyanastra-30557.firebaseapp.com",
+  projectId: "gyanastra-30557",
+  storageBucket: "gyanastra-30557.firebasestorage.app",
+  messagingSenderId: "691234854780",
+  appId: "1:691234854780:web:c1213c6b4e99ee4815f6f9",
+  measurementId: "G-96793NR52M"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+// Initialize Firestore
+const db = firebase.firestore();
